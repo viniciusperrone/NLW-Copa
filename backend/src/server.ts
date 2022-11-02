dc43e46ba07489be0ff0c357f1ea2590f6479fe4
Vinicius Perrone <perronevinicius@gmail.com>
2022-11-02 18:23:41 -0300

    feat: fineshed backend setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,27 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
+
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
 async function boostrap() {
   const fastify = Fastify({
     logger: true
   });
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  await fastify.register(cors, {
+    origin: true
   });
-  
-  await fastify.listen({ port: 3333 });
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count();
+    
+    return { count }
+  });
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 boostrap();
